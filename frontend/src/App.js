59cda1afb477ceb08222cc5698cb0812edd81e13
SmsS <smss.lite@gmail.com>
2022-01-02 15:02:25 +0330

    Add temeplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,88 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+/// use toast for error handeling
+
+const STATES = {
+  'register': 1,
+  'login': 2,
+  'dashboard': 3,
+}
+
+const NOTE_STATE = {
+  'none': 0,
+  'show': 1,
+  'edit': 2,
+  'post': 3,
+}
+
+function Register() {
+  /// username
+  /// password
+}
+
+function Login() {
+  /// username
+  /// password
+  /// link to go to register
+}
+
+function ListOfNotes({updateNoteId, updateNoteState}) {
+  /// list of notes title
+  /// delete button
+  /// edit button
+  /// show button
+}
+
+function ShowNote({noteId, noteState}) {
+  /// close button
+  /// delete button ?
+  /// edit button ?
+}
+
+function EditOrPostNote({onSend}) {
+  /// component for editing or posting note
+  /// send button
+}
+
+function Dashboard() {
+  /// logout
+  /// ListOfNotes
+  /// ShowNote if a note selected
+  /// EditOrPostNote if edit or post note
+  const [noteId, updateNoteId] =  useState(null);
+  const [noteState, updateNoteState] = useState(NOTE_STATE.none)
+  const onSend = () => {};
+  return (
+    <>
+      {
+        <ListOfNotes
+          updateNoteId={updateNoteId}
+          updateNoteState={updateNoteState}
+        />
+      }
+      {noteState == NOTE_STATE.show && <ShowNote noteId={noteId}  noteState={noteState} updateNoteState={updateNoteState}/>}
+      {
+        (noteState == NOTE_STATE.edit || noteState == NOTE_STATE.post) &&
+        <EditOrPostNote 
+          noteId={noteId}
+          noteState={noteState}
+          updateNoteState={updateNoteState}
+        />
+      }
+    </>
+  )
+}
+
 
 function App() {
+  const [state, updateState] = useState(STATES.login);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {state == STATES.login && <Login />}
+      {state == STATES.register && <Register />}
+      {state == STATES.dashboard && <Dashboard />}
+    </>
   );
 }
 
