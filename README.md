cf3ab94a9e9fdd3b3b732f8619a0313b03fce066
SmsS <smss.lite@gmail.com>
2022-01-07 20:31:11 +0330

    Add db

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # KeepIt
+
+
+
+```
+vscode-proto3
+
+
+sudo apt install -y protobuf-compiler
+go mod init github.com/SmsS4/grpc
+go install google.golang.org/protobuf/cmd/protoc-gen-go
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+export PATH="$PATH:$(go env GOPATH)/bin"
+protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=.  --go-grpc_opt=paths=source_relative route_guide.proto
+protoc --go_out=plugins=grpc:chat srvc.proto 
+
+```
--- cache/main.go
+++ cache/main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type DBConfig struct {
+	host     string
+	username string
+	password string
+	name     string
+	port     int
+}
+
+type Config struct {
+	db DBConfig
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+}
+
+func getDBConfig(data map[string]string) DBConfig {
+	var config DBConfig
+	config.host = data["db_host"]
+	config.username = data["db_username"]
+	config.password = data["db_password"]
+	config.name = data["db_name"]
+	db_port, err := strconv.Atoi(data["db_port"])
+	checkError(err)
+	config.port = db_port
+	return config
+}
+
+func getConfig(configPath string) Config {
+	log.Print("Get config")
+	configFile, err := ioutil.ReadFile("config.yml")
+	checkError(err)
+	configMap := make(map[string]string)
+	checkError(yaml.Unmarshal(configFile, &configMap))
+	var config Config
+	config.db = getDBConfig(configMap)
+	log.Printf(
+		"DB Config: %s:%d user:%s name:%s",
+		config.db.host,
+		config.db.port,
+		config.db.username,
+		config.db.name,
+	)
+	return config
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	log.Print("Starting server...")
+	config := getConfig("config.yml")
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
+		config.db.host,
+		config.db.username,
+		config.db.password,
+		config.db.name,
+		config.db.port,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	checkError(err)
 }
