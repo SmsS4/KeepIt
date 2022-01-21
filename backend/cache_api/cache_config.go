a55a41b49aefad8159c10602b9392ce33896c90f
SmsS <smss.lite@gmail.com>
2022-01-22 00:31:12 +0330

    Don't import from cache anymore

--- backend/cache_api/cache_config.go
+++ backend/cache_api/cache_config.go
@@ -1,18 +1,54 @@
 package cache_api
 
 import (
+	"fmt"
 	"log"
+	"sort"
+	"strconv"
+	"strings"
 
-	server "github.com/SmsS4/KeepIt/cache/cache_server"
+	"github.com/SmsS4/KeepIt/cache/utils"
 )
 
+type Instance struct {
+	Ip       string
+	Port     int
+	Priority int
+}
+
+func ParseInsances(data string) []Instance {
+	instancesStrings := strings.Split(data, ",")
+	instances := make([]Instance, len(instancesStrings))
+	for i := 0; i < len(instancesStrings); i++ {
+		ipAndPort := strings.Split(instancesStrings[i], ":")
+		ip := ipAndPort[0]
+		port, err := strconv.Atoi(ipAndPort[1])
+		utils.CheckError(err)
+		priority, err := strconv.Atoi(ipAndPort[2])
+		utils.CheckError(err)
+		log.Printf("Instance %d is on %s:%d with priority %d", i, ip, port, priority)
+		instances[i] = Instance{
+			Ip:       ip,
+			Port:     port,
+			Priority: priority,
+		}
+	}
+	sort.Slice(instances, func(i, j int) bool {
+		return instances[i].Priority < instances[j].Priority
+	})
+	return instances
+}
+func (instance *Instance) GetUrl() string {
+	return fmt.Sprintf("%s:%d", instance.Ip, instance.Port)
+}
+
 type CacheConfig struct {
-	instances []server.Instance
+	instances []Instance
 }
 
 func GetCacheConfig(data map[string]string) CacheConfig {
 	log.Printf("Getting cache config %s", data)
 	return CacheConfig{
-		instances: server.ParseInsances(data["instances"]),
+		instances: ParseInsances(data["instances"]),
 	}
 }
