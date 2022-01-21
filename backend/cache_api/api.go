04cc89f5a64fe743dc0e0806577cab1a6a8b4f23
alireza <alirezzzhp1378@gmail.com>
2022-01-21 21:37:15 +0330

    backend main

---
+++ backend/cache_api/api.go
@@ -0,0 +1,146 @@
+package cache_api
+
+import (
+	"context"
+	"log"
+
+	server "github.com/SmsS4/KeepIt/cache/cache_server"
+	"github.com/SmsS4/KeepIt/cache/utils"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type CacheApi struct {
+	config          CacheConfig
+	currentInstance string
+}
+
+func CreateApi(config CacheConfig) *CacheApi {
+	api := &CacheApi{
+		config: config,
+	}
+	api.currentInstance = config.instances[0].GetUrl()
+	return api
+}
+
+func (api *CacheApi) createConnection(url string) (server.CacheServiceClient, error) {
+	log.Printf("Create connection to %s", url)
+	var conn *grpc.ClientConn
+	cert, err := server.LoadTLSCredentials()
+	utils.CheckError(err)
+	conn, err = grpc.Dial(
+		url,
+		grpc.WithTransportCredentials(cert),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return server.NewCacheServiceClient(conn), err
+}
+
+func (api *CacheApi) findConnection() string {
+	for _, instance := range api.config.instances {
+		log.Printf("Connecting to %s", instance.GetUrl())
+		connection, err := api.createConnection(instance.GetUrl())
+		if err == nil {
+			response, err := connection.Check(
+				context.Background(),
+				&server.Nil{},
+			)
+			log.Printf("Response is %s", response)
+			if err == nil {
+				log.Print("Connected")
+				return instance.GetUrl()
+			}
+		}
+		log.Print("Failed")
+	}
+	log.Fatal("Could not find any connection")
+	return ""
+}
+
+func (api *CacheApi) FindNewActive(err error) {
+	st := status.Convert(err)
+	if st.Code() == codes.Unavailable {
+		api.currentInstance = api.findConnection()
+	} else {
+		log.Fatalf("Error %s", err)
+	}
+
+}
+
+func (api *CacheApi) Get(key string) string {
+	conn, err := api.createConnection(
+		api.currentInstance,
+	)
+	utils.CheckError(err)
+	response, err := conn.Get(
+		context.Background(),
+		&server.Key{
+			Key: key,
+		},
+	)
+	log.Printf("Get response %s", response)
+	st := status.Convert(err)
+	if st.Code() == codes.Unavailable {
+		log.Printf("network error")
+		api.FindNewActive(err)
+		return api.Get(key)
+	}
+
+	if err != nil {
+		return ""
+	}
+	if response.ActiveIp != "" {
+		api.currentInstance = response.ActiveIp
+		return api.Get(key)
+	}
+	return response.Value
+}
+
+func (api *CacheApi) Put(key string, value string) *server.OprationResult {
+	conn, err := api.createConnection(
+		api.currentInstance,
+	)
+	utils.CheckError(err)
+	response, err := conn.Put(
+		context.Background(),
+		&server.KeyValue{
+			Key:   key,
+			Value: value,
+		},
+	)
+	log.Printf("Put response %s", response)
+	if err != nil {
+		api.FindNewActive(err)
+		return api.Put(key, value)
+	}
+	if response.ActiveIp != "" {
+		api.currentInstance = response.ActiveIp
+		return api.Put(key, value)
+	}
+	return response
+}
+
+func (api *CacheApi) Clear() *server.OprationResult {
+	conn, err := api.createConnection(
+		api.currentInstance,
+	)
+	utils.CheckError(err)
+	response, err := conn.Clear(
+		context.Background(),
+		&server.Nil{},
+	)
+	log.Printf("Clear response %s", response)
+	if err != nil {
+		api.FindNewActive(err)
+		return api.Clear()
+	}
+	if response.ActiveIp != "" {
+		api.currentInstance = response.ActiveIp
+		return api.Clear()
+	}
+
+	return response
+}
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,246 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+	"strconv"
+	"strings"
+	"github.com/SmsS4/KeepIt/backend/cache_api"
+	"github.com/SmsS4/KeepIt/cache/utils"
+	jwt_lib "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/contrib/jwt"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/yaml.v2"
+	"math/rand"
+
+)
+
+type Config struct {
+	CacheApi cache_api.CacheConfig
+}
+
+var (
+	mysupersecretpassword = "unicornsAreAwesome"
+)
+
+type UserInput struct {
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type NewNoteInput struct {
+	Note string `json:"note" binding:"required"`
+}
+
+type UpdateNoteInput struct {
+	Note_id string `json:"note_id" binding:"required"`
+	Note string `json:"note" binding:"required"`
+}
+
+func GenerateId() string{
+	res := strconv.Itoa(rand.Intn(99999999-10000000) + 10000000)
+	return res
+}
+
+func ParseToken(tokenStr string) string {
+
+	//creating the token
+	token, _ := jwt_lib.Parse(tokenStr, func(token *jwt_lib.Token) (interface{}, error) {
+		return mysupersecretpassword, nil
+	})
+
+	// extracting username
+	if claims, ok := token.Claims.(jwt_lib.MapClaims); ok && token.Valid {
+		username := claims["username"].(string)
+		return username
+	}
+	return ""
+}
+
+func getConfig(configPath string) Config {
+	log.Print("Getting config")
+	configFile, err := ioutil.ReadFile(configPath)
+	utils.CheckError(err)
+	configMap := make(map[string]map[string]string)
+	utils.CheckError(yaml.Unmarshal(configFile, &configMap))
+	return Config{
+		CacheApi: cache_api.GetCacheConfig(configMap["cache"]),
+	}
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+
+
+	config := getConfig(os.Args[1])
+	kash := cache_api.CreateApi(config.CacheApi)
+	r := gin.Default()
+	public := r.Group("/pub")
+
+	public.POST("/register", func(c *gin.Context) {
+		var input UserInput
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+		
+		if len(input.Username) == 0 || len(input.Username) > 55{
+			c.JSON(400, gin.H{"error": "username length error"})
+			return
+		}
+
+		if len(input.Password) == 0 {
+			c.JSON(400, gin.H{"error": "password cant be empty"})
+			return
+		}
+		if kash.Get(input.Username) != "" {
+			c.JSON(400, gin.H{"error": "user already exists"})
+			return
+		}
+
+		kash.Put(input.Username, input.Password)
+
+		c.JSON(200, gin.H{"message": "user registered succssessfully"})
+
+	})
+
+	public.GET("/login", func(c *gin.Context) {
+		var input UserInput
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		if kash.Get(input.Username) == "" || kash.Get(input.Username) != input.Password {
+			c.JSON(404, gin.H{"error": "invalid username or password"})
+			return
+		}
+
+		// Create the token
+		token := jwt_lib.New(jwt_lib.GetSigningMethod("HS256"))
+		// Set some claims
+		token.Claims = jwt_lib.MapClaims{
+			"username":  input.Username,
+			"exp": time.Now().Add(time.Hour * 1).Unix(),
+		}
+		// Sign and get the complete encoded token as a string
+		tokenString, err := token.SignedString([]byte(mysupersecretpassword))
+		if err != nil {
+			c.JSON(500, gin.H{"message": "Could not generate token"})
+		}
+		c.JSON(200, gin.H{"token": tokenString})
+	})
+
+	private := r.Group("/private")
+	private.Use(jwt.Auth(mysupersecretpassword))
+
+	private.POST("/new_note", func(c *gin.Context) {
+		var input NewNoteInput
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		if len(input.Note) == 0 || len(input.Note) > 2048 {
+			c.JSON(400, gin.H{"error": "note length error"})
+			return
+		}
+
+		auth := c.Request.Header.Get("Authorization")
+		if auth == "" {
+			c.String(400, "No Authorization header provided")
+			c.Abort()
+			return
+		}
+		token := strings.TrimPrefix(auth, "Bearer ")
+		username := ParseToken(token)
+		note_id := username + "$" + GenerateId()
+		kash.Put(note_id, input.Note)
+		c.JSON(500, gin.H{"note_id": note_id})
+	})
+
+	private.PUT("/update_note", func(c *gin.Context) {
+		var input UpdateNoteInput
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		auth := c.Request.Header.Get("Authorization")
+		if auth == "" {
+			c.String(400, "No Authorization header provided")
+			c.Abort()
+			return
+		}
+		token := strings.TrimPrefix(auth, "Bearer ")
+		username := ParseToken(token)
+
+		if username != strings.Split(input.Note_id, "$")[0] && username != "admin"{
+			c.JSON(400, gin.H{"error": "you are not authorized to uppdate note"})
+			return
+		}
+
+		if kash.Get(input.Note_id) == ""{
+			c.JSON(400, gin.H{"error": "note doesnt exist"})
+			return
+		}
+
+		if len(input.Note) == 0 || len(input.Note) > 2048{
+			c.JSON(400, gin.H{"error": "note length error"})
+			return
+		}
+
+		kash.Put(input.Note_id, input.Note)
+		c.JSON(200, gin.H{"message": "note changed succssessfully"})
+	})
+
+	private.GET("/get_note/:note_id", func(c *gin.Context) {
+		note_id := c.Param("note_id")
+		auth := c.Request.Header.Get("Authorization")
+		if auth == "" {
+			c.String(400, "No Authorization header provided")
+			c.Abort()
+			return
+		}
+		token := strings.TrimPrefix(auth, "Bearer ")
+		username := ParseToken(token)
+
+		if username != strings.Split(note_id, "$")[0] && username != "admin"{
+			c.JSON(400, gin.H{"error": "you are not authorized to read note"})
+			return
+		}
+		if kash.Get(note_id) == ""{
+			c.JSON(400, gin.H{"error": "note doesn't exist"})
+			return
+		}
+		c.JSON(200, gin.H{"note": kash.Get(note_id)})
+	})
+
+	private.DELETE("/delete_note/:note_id", func(c *gin.Context) {
+		note_id := c.Param("note_id")
+		auth := c.Request.Header.Get("Authorization")
+		if auth == "" {
+			c.String(400, "No Authorization header provided")
+			c.Abort()
+			return
+		}
+		token := strings.TrimPrefix(auth, "Bearer ")
+		username := ParseToken(token)
+
+		if username != strings.Split(note_id, "$")[0] && username != "admin"{
+			c.JSON(400, gin.H{"error": "you are not authorized to delete note"})
+			return
+		}
+		if kash.Get(note_id) == ""{
+			c.JSON(400, gin.H{"error": "note doesn't exist"})
+			return
+		}
+		kash.Put(note_id, "")
+		c.JSON(200, gin.H{"message": "note deleted successfully")
+	})
+	
+	r.Run("localhost:8080")
+
+
 }
